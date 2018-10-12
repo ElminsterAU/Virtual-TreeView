8625b3704bf1c2dc3cbbb0b6b70ed584fe34030d
eFrysTon <github@thorsten.engler.id.au>
2018-10-13 00:09:02 +1000

    When reinitializing a node, if AutoFreeOnCollapse is set and the node is not Expanded, remove existing children instead of reinitializing them

--- Source/VirtualTrees.pas
+++ Source/VirtualTrees.pas
@@ -32058,7 +32058,12 @@ begin
   end;
 
   if Recursive then
-    ReinitChildren(Node, True);
+    {>>>}
+    if (toAutoFreeOnCollapse in TreeOptions.AutoOptions) and not Expanded[Node] then
+      DeleteChildren(Node)
+    else
+	{<<<}
+      ReinitChildren(Node, True);
 end;
 
 //----------------------------------------------------------------------------------------------------------------------
